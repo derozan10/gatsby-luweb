5ddbc1e852836e85ed3df59348c524ba73b1bd2e
Lucas Van Remoortere <lucas.vanremoortere@gmail.com>
2024-10-15 11:22:22 +0200

    chore(node): node 20

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,7 +30,6 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-styled-components',
-    'gatsby-plugin-sass',
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
     {
